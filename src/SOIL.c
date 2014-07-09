kbranigan/Simple-OpenGL-Image-Library

commit 4fd65af914ac33ebf39fa25278733a936a14d50d

Cherrypicked archSeer's Android support additions Changes picked from these commits: 0ecd16650bc32b3b7597014b01cd318cfe22b9a8 d64998d54ef9771d410c4960a0f0a8e443de841a 9ff5f113a0891386e66793c033951001a25cd246 Closes kbranigan/Simple-OpenGL-Image-Library#4

src/SOIL.c
@@ -25,6 +25,9 @@
 	#include <OpenGL/gl.h>
 	#include <Carbon/Carbon.h>
 	#define APIENTRY
+#elif defined(__ANDROID__)
+	#include <GLES/gl.h>
+	#define APIENTRY
 #else
 	#include <GL/gl.h>
 	#include <GL/glx.h>
@@ -1343,7 +1346,7 @@ unsigned int
 		} else
 		{
 			/*	unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;	*/
-			unsigned int clamp_mode = GL_CLAMP;
+			unsigned int clamp_mode = GL_CLAMP_TO_EDGE;
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_S, clamp_mode );
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_T, clamp_mode );
 			if( opengl_texture_type == SOIL_TEXTURE_CUBE_MAP )
@@ -1810,7 +1813,7 @@ unsigned int SOIL_direct_load_DDS_from_memory(
 		} else
 		{
 			/*	unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;	*/
-			unsigned int clamp_mode = GL_CLAMP;
+			unsigned int clamp_mode = GL_CLAMP_TO_EDGE;
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_S, clamp_mode );
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_T, clamp_mode );
 			glTexParameteri( opengl_texture_type, SOIL_TEXTURE_WRAP_R, clamp_mode );
@@ -1879,6 +1882,9 @@ int query_NPOT_capability( void )
 		if(
 			(NULL == strstr( (char const*)glGetString( GL_EXTENSIONS ),
 				"GL_ARB_texture_non_power_of_two" ) )
+		&&
+			(NULL == strstr( (char const*)glGetString( GL_EXTENSIONS ),
+				"GL_OES_texture_npot" ) )
 			)
 		{
 			/*	not there, flag the failure	*/
@@ -1935,6 +1941,9 @@ int query_cubemap_capability( void )
 			(NULL == strstr( (char const*)glGetString( GL_EXTENSIONS ),
 				"GL_EXT_texture_cube_map" ) )
 			)
+		#ifdef GL_ES_VERSION_2_0
+		&& (false) /* GL ES 2.0 supports cubemaps, always enable */
+		#endif
 		{
 			/*	not there, flag the failure	*/
 			has_cubemap_capability = SOIL_CAPABILITY_NONE;
@@ -1994,6 +2003,8 @@ int query_DXT_capability( void )
 				CFRelease( bundleURL );
 				CFRelease( extensionName );
 				CFRelease( bundle );
+			#elif defined(__ANDROID__)
+				ext_addr = (P_SOIL_GLCOMPRESSEDTEXIMAGE2DPROC)(glCompressedTexImage2D);
 			#else
 				ext_addr = (P_SOIL_GLCOMPRESSEDTEXIMAGE2DPROC)
 						glXGetProcAddressARB

commit 773c8e9393404764ac1c335583952c6811e80a36

Fix Windows compile issue caused by 4fd65af Specifically, Windows' gl.h only has definitions up to OpenGL 1.1 but GL_CLAMP_TO_EDGE was introduced in OpenGL 1.2. So, use SOIL's definition instead.

src/SOIL.c
@@ -1350,8 +1350,7 @@ unsigned int
 			check_for_GL_errors( "GL_TEXTURE_WRAP_*" );
 		} else
 		{
-			/*	unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;	*/
-			unsigned int clamp_mode = GL_CLAMP_TO_EDGE;
+			unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_S, clamp_mode );
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_T, clamp_mode );
 			if( opengl_texture_type == SOIL_TEXTURE_CUBE_MAP )
@@ -1817,8 +1816,7 @@ unsigned int SOIL_direct_load_DDS_from_memory(
 			glTexParameteri( opengl_texture_type, SOIL_TEXTURE_WRAP_R, GL_REPEAT );
 		} else
 		{
-			/*	unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;	*/
-			unsigned int clamp_mode = GL_CLAMP_TO_EDGE;
+			unsigned int clamp_mode = SOIL_CLAMP_TO_EDGE;
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_S, clamp_mode );
 			glTexParameteri( opengl_texture_type, GL_TEXTURE_WRAP_T, clamp_mode );
 			glTexParameteri( opengl_texture_type, SOIL_TEXTURE_WRAP_R, clamp_mode );